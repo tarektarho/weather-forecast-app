c78604c075531b11826cb191bf5eb92466ae61bf
Tarek Tarho <tarek_tarho@hotmail.com>
2023-08-10 21:41:24 +0200

    Create airPollution.ts

---
+++ src/types/airPollution.ts
@@ -0,0 +1,30 @@
+interface ComponentsInfo {
+  co: number
+  no: number
+  no2: number
+  o3: number
+  so2: number
+  pm2_5: number
+  pm10: number
+  nh3: number
+}
+
+interface AirQualityInfo {
+  aqi: number
+}
+
+interface AirPollutionItem {
+  main: AirQualityInfo
+  components: ComponentsInfo
+  dt: number
+}
+
+interface AirPollutionData {
+  coord: {
+    lon: number
+    lat: number
+  }
+  airPoList: AirPollutionItem
+}
+
+export default AirPollutionData
