831c4619a05eb16cd68f718b477a8445473d83a1
Tarek Tarho <tarek_tarho@hotmail.com>
2023-08-10 21:41:06 +0200

    Implement services tests

---
+++ src/services/__test__/airPollution.test.ts
@@ -0,0 +1,50 @@
+import { getAirPollutionByLatLon } from "../airPollution"
+import { ERROR_INVALID_LAT_LON } from "../../utils/constants"
+
+export const fetchAirPolutionMockedResponse = {
+  coord: {
+    lat: 52.1004,
+    lon: 5.0563,
+  },
+  list: [
+    {
+      components: {
+        co: 247,
+        nh3: 6.33,
+        no: 16.76,
+        no2: 20.22,
+        o3: 20.56,
+        pm10: 16.76,
+        pm2_5: 12.91,
+        so2: 2.89,
+      },
+      dt: 1691654686,
+      main: {
+        aqi: 2,
+      },
+    },
+  ],
+}
+
+describe("airPollution service", () => {
+  const latitude = 52.1004105
+  const longitude = 5.0563431
+
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  test("throws an error for when lat & lon are invalid", async () => {
+    await expect(
+      async () => await getAirPollutionByLatLon(0, 0),
+    ).rejects.toThrowError(ERROR_INVALID_LAT_LON)
+  })
+  test("load airPollution data as expected", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(fetchAirPolutionMockedResponse))
+    const weatherServiceResponse = await getAirPollutionByLatLon(
+      latitude,
+      longitude,
+    )
+    expect(weatherServiceResponse).toEqual(fetchAirPolutionMockedResponse)
+  })
+})
---
+++ src/services/__test__/weather.test.ts
@@ -0,0 +1,97 @@
+import {
+  ERROR_INVALID_CITY,
+  ERROR_INVALID_LAT_LON,
+} from "../../utils/constants"
+import { getWeatherByLatLon, getWeatherByCity } from "../weather"
+
+export const weatherServiceMockedResponse = {
+  base: "stations",
+  clouds: {
+    all: 66,
+  },
+  cod: 200,
+  coord: {
+    lat: 52,
+    lon: 5.25,
+  },
+  dt: 1691658720,
+  id: 2745909,
+  main: {
+    feels_like: 292.13,
+    humidity: 69,
+    pressure: 1021,
+    temp: 292.35,
+    temp_max: 293.75,
+    temp_min: 291.16,
+  },
+  name: "Provincie Utrecht",
+  sys: {
+    country: "NL",
+    id: 2011892,
+    sunrise: 1691640861,
+    sunset: 1691694876,
+    type: 2,
+  },
+  timezone: 7200,
+  visibility: 10000,
+  weather: [
+    {
+      description: "broken clouds",
+      icon: "04d",
+      id: 803,
+      main: "Clouds",
+    },
+  ],
+  wind: {
+    deg: 219,
+    gust: 1.34,
+    speed: 1.34,
+  },
+}
+
+describe("forecast service", () => {
+  const latitude = 25.9445
+  const longitude = -80.1747
+  const city = "Utrecht"
+  const invalidCity = "Invalid City"
+
+  const weatherApiCityInvalidMockedResponse = {
+    cod: "404",
+    message: "city not found",
+  }
+
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+
+  test("should throw an error when city is not found", async () => {
+    await expect(() => getWeatherByCity("")).rejects.toThrowError(
+      ERROR_INVALID_CITY,
+    )
+  })
+
+  it("returns an error if invalid city", async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify(weatherApiCityInvalidMockedResponse),
+    )
+    const weatherApiResponse = await getWeatherByCity(invalidCity)
+    expect(weatherApiResponse).toEqual(weatherApiCityInvalidMockedResponse)
+  })
+
+  test("should throw an error for invalid lon & lat", async () => {
+    await expect(() => getWeatherByLatLon(0, 0)).rejects.toThrowError(
+      ERROR_INVALID_LAT_LON,
+    )
+  })
+
+  test("should return weather data based on city name", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(weatherServiceMockedResponse))
+    const response = await getWeatherByCity(city)
+    expect(response).toEqual(weatherServiceMockedResponse)
+  })
+  test("should return weather data based on lon & lat", async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(weatherServiceMockedResponse))
+    const response = await getWeatherByLatLon(latitude, longitude)
+    expect(response).toEqual(weatherServiceMockedResponse)
+  })
+})
